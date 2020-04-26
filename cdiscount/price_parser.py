b3c85b0bc56a01d4861283fce3d92535ae6c9d40
Antoine Chassagne <antoine.chassagne@me.com>
2020-04-26 11:20:14 +0200

    :sparkles: Add price_parser module

--- cdiscount/price_parser.py
+++ cdiscount/price_parser.py
@@ -1,3 +1,15 @@
+import requests
+from bs4 import BeautifulSoup
+
 def parse_price(sku: str) -> float:
-  return 9.99
+  url = "https://www.cdiscount.com/f-0-" + sku + ".html"
+  request = requests.get(url)
+
+  if (request.status_code != 200):
+    return 0
+  
+  soup = BeautifulSoup(request.text, "html.parser")
+  price = float(soup.find_all("span", {"class":"fpPrice price jsMainPrice jsProductPrice hideFromPro"})[0]["content"])
+
+  return price
   
