b40d959b0b91d9dd908272a4989ba4c38a844379
Antoine Chassagne <antoine.chassagne@me.com>
2020-04-26 13:00:47 +0200

    :building_construction: Add the packaging

---
+++ cdiscount/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='cdiscount',
+    version = '0.1',
+    description = 'Return the price for a given Cdiscount\'s product reference.',
+    url = 'https://github.com/antoinechassagne/price-parser',
+    packages = find_packages(),
+    install_requires = [
+        'requests',
+        'beautifulsoup4'
+    ],
+    python_requires='>=3.7'
+    )
