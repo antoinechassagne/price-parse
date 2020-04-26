68a24a75a10ba06b76a9e12a06f68fa96ecc1c04
Antoine Chassagne <antoine.chassagne@me.com>
2020-04-26 11:45:01 +0200

    :bulb: Add documentation for price_parser module

--- cdiscount/price_parser.py
+++ cdiscount/price_parser.py
@@ -1,12 +1,23 @@
+from typing import Union
 import requests
 from bs4 import BeautifulSoup
 
-def parse_price(sku: str) -> float:
+""" 
+This function extracts the price from a Cdiscount product page. 
+    
+Parameters: 
+    sku (str): The product reference
+    
+Returns: 
+    float: The product price
+    bool: False if something went wrong
+"""
+def parse_price(sku: str) -> Union[float, bool]:
     url = "https://www.cdiscount.com/f-0-" + sku + ".html"
     request = requests.get(url)
 
     if (request.status_code != 200):
-    return 0
+        return False
 
     soup = BeautifulSoup(request.text, "html.parser")
     price = float(soup.find_all("span", {"class":"fpPrice price jsMainPrice jsProductPrice hideFromPro"})[0]["content"])
