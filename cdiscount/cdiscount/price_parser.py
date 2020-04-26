1ae6e51f820fac7d7a2e4e8e1fc914fc04cded1d
Antoine Chassagne <antoine.chassagne@me.com>
2020-04-26 14:22:04 +0200

    :bug: Handle edge cases

--- cdiscount/cdiscount/price_parser.py
+++ cdiscount/cdiscount/price_parser.py
@@ -13,6 +13,9 @@ Returns:
     bool: False if something went wrong
 """
 def parse_price(sku: str) -> Union[float, bool]:
+    if not isinstance(sku, str):
+        return False
+
     url = "https://www.cdiscount.com/f-0-" + sku + ".html"
     request = requests.get(url)
 
@@ -20,7 +23,11 @@ def parse_price(sku: str) -> Union[float, bool]:
         return False
 
     soup = BeautifulSoup(request.text, "html.parser")
-    price = float(soup.find_all("span", {"class":"fpPrice price jsMainPrice jsProductPrice hideFromPro"})[0]["content"])
+    price_box = soup.find_all("span", {"class":"fpPrice price jsMainPrice jsProductPrice hideFromPro"})
 
+    if (not len(price_box)):
+        return False
+    
+    price = float(price_box[0]["content"])
     return price
   
