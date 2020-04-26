ebcb00cb1006f689d25603256ee13c588248e09b
Antoine Chassagne <antoine.chassagne@me.com>
2020-04-26 11:29:13 +0200

    :art: Format code with 4 spaces

--- cdiscount/price_parser.py
+++ cdiscount/price_parser.py
@@ -2,14 +2,14 @@ import requests
 from bs4 import BeautifulSoup
 
 def parse_price(sku: str) -> float:
-  url = "https://www.cdiscount.com/f-0-" + sku + ".html"
-  request = requests.get(url)
+    url = "https://www.cdiscount.com/f-0-" + sku + ".html"
+    request = requests.get(url)
 
-  if (request.status_code != 200):
+    if (request.status_code != 200):
     return 0
-  
-  soup = BeautifulSoup(request.text, "html.parser")
-  price = float(soup.find_all("span", {"class":"fpPrice price jsMainPrice jsProductPrice hideFromPro"})[0]["content"])
 
-  return price
+    soup = BeautifulSoup(request.text, "html.parser")
+    price = float(soup.find_all("span", {"class":"fpPrice price jsMainPrice jsProductPrice hideFromPro"})[0]["content"])
+
+    return price
   
